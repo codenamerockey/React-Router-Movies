f49ece956a18fcb602d45a77aa4f1a5e4213d43e
Rockey Simmons <codenamerockey@gmail.com>
2019-08-19 16:46:10 -0700

    updated files to link to movies file when clicked

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
+        .get("http://localhost:5000/api/movies")
         .then(response => {
           setMovies(response.data);
+          console.log(response.data);
         })
         .catch(error => {
-          console.error('Server Error', error);
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
@@ -25,19 +27,22 @@ const MovieList = props => {
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars, id } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <NavLink to={`/movies/${id}`}>
+        <h2>{title}</h2>
+      </NavLink>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+
       <h3>Actors</h3>
 
       {stars.map(star => (
